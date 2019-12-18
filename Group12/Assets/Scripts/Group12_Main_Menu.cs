81ad0fa4db3efb50942d1d3508ee4e28c4c84be0
Toc-Project-Fall-19 <tocprojectfall19@gmail.com>
2019-12-19 01:03:58 +0500

    ...

---
+++ Group12/Assets/Scripts/Group12_Main_Menu.cs
@@ -0,0 +1,57 @@
+<<<<<<< HEAD
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Group12_Main_Menu : MonoBehaviour
+{
+    public void palindrom()
+    {
+        SceneManager.LoadScene("Group12_L4_Palidrome");
+    }
+    public void adil()
+    {
+        SceneManager.LoadScene("Group12_L1_Adil");
+    }
+    public void iqra()
+    {
+        SceneManager.LoadScene("Group12_L2_Iqra");
+    }
+    public void areesha()
+    {
+=======
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Group12_Main_Menu : MonoBehaviour
+{
+    public void palindrom()
+    {
+        SceneManager.LoadScene("Group12_L4_Palidrome");
+    }
+    public void adil()
+    {
+        SceneManager.LoadScene("Group12_L1_Adil");
+    }
+    public void iqra()
+    {
+        SceneManager.LoadScene("Group12_L2_Iqra");
+    }
+    public void areesha()
+    {
+>>>>>>> cf3c9704f9eef860e0510422a8e01cd91bea37bd
+        SceneManager.LoadScene("Group12_L3_Areesha");
+	}
+	public void exit()
+	{
+		Application.Quit();
+<<<<<<< HEAD
+	}
+}
+=======
+	}
+}
+>>>>>>> cf3c9704f9eef860e0510422a8e01cd91bea37bd
